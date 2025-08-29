96ac8755e1c8af9eb592e78d78069b6266383b31
diok4 <webdeveloperpath21@gmail.com>
2025-08-29 16:14:23 +0500

    mainpage import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { MainPage } from "../pages/main/ui/main";
+import { MainPage } from "../pages/main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
