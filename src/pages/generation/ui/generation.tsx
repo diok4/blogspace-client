646cda99e63984f7f7aa275419ade0de335740c6
diok4 <webdeveloperpath21@gmail.com>
2025-08-29 16:14:54 +0500

    created

---
+++ src/pages/generation/ui/generation.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import styles from "./generation.module.css";
+
+export const GenerationPage: FC = () => {
+  return (
+    <div>
+      <h1>GenerationPage</h1>
+    </div>
+  );
+};
---
+++ src/pages/settings/ui/settings.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import styles from "./settings.module.css";
+
+export const SettingsPage: FC = () => {
+  return (
+    <div>
+      <h1>SettingsPage</h1>
+    </div>
+  );
+};
