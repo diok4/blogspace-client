d6ea022e10d9070e6529a1262908a910afb94e4c
diok4 <webdeveloperpath21@gmail.com>
2025-08-30 01:23:39 +0500

    basic register form

--- src/pages/auth/register/ui/register.tsx
+++ src/pages/auth/register/ui/register.tsx
@@ -1,10 +1,26 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import styles from "./register.module.css";
+import { Input } from "@/shared/ui/input";
 
 export const RegisterPage: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      <h1>RegisterPage</h1>
+    <div className={styles.authContainer}>
+      <Input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        name="email"
+        placeholder="email"
+      />
+      <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        placeholder="password"
+      />
     </div>
   );
 };
